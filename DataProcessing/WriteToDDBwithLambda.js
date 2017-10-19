4b15df1c30571997b14418c8738882735162dba6
Riccardo La Rosa <r.larosa@gmail.com>
2017-10-19 09:29:37 -0400

    changed README.md

--- DataProcessing/WriteToDDBwithLambda.js
+++ DataProcessing/WriteToDDBwithLambda.js
@@ -1,125 +1,42 @@
-// use this function when you have a file in S3
-// made of JSON elements and want to load that
-// info in DynamoDB
-// although you should be looking at
-// `AWS.DynamoDB.DocumentClient()` as an alternative
-// Since this assumes you have a lot of JSON objects
-// it's using DynamoDB.batchWrite(), make sure
-// you read the documentation:
-// http://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_BatchWriteItem.html
+// simple and straightforward function to write to DynamoDB
+// a single  object `dynamoItem` using `AWS.DynamoDB.DocumentClient()`
+// DcoumentClient  annotates native JavaScript types supplied as
+// input parameters, as well as converts annotated response data
+// to native JavaScript types.
 
-
-'use strict';
-
+// dependencies
 const AWS = require('aws-sdk');
-
-const S3 = new AWS.S3();
-const DynamoDB = new AWS.DynamoDB.DocumentClient();
-
+// need util just to print out the event object
+const util = require('util');
 // make sure you add an environment TABLE_NAME to lambda
-const TABLE_NAME = process.env.TABLE_NAME;
-const THROTTLING_ERRORS = [
-    'ProvisionedThroughputExceededException',
-    'ThrottlingException',
-];
+const tableName = process.env.RIDER_PHOTOS_DDB_TABLE;
+
+const docClient = new AWS.DynamoDB.DocumentClient({
+    region: process.env.AWS_REGION
+});
 
 exports.handler = (event, context, callback) => {
+    console.log("Reading input from event:\n", util.inspect(event, {depth: 5}));
 
-    // create an array of promises to upload
-    // a bunch of JSON records in S3 to DynamoDB
-    const promises = event.Records.map((record) => {
-        const params = {
-            Bucket: record.s3.bucket.name,
-            Key: record.s3.object.key,
-        };
-        // this is the meat and potato of the fuction:
-        // read objects in S3, format the request, write it to DynamoDB
-        return S3.getObject(params).promise()
-            .then(data => buildRequests(data.Body))
-            .then(items => processRequests(items));
-    });
-    // takes an array of promises and returns a Promise
-    // that resolves when all the 'promises' have resolved
-    Promise.all(promises)
-        .then(() => callback(null, `Processed ${event.Records.length} file(s)`))
-        .catch(err => callback(err));
-};
-
-// Take the json input and transform it (by adding StatusTime)
-// to appropriate format for DynamoDB
-function buildRequests(buf) {
-    // create a new array of type "item" by parsing the input file
-    const requestItems = buf.toString().trim().split('\n').map((json) => {
-
-        // you should check you are parsing a correct json record
-        // and handle any errors
-        const item = JSON.parse(json);
-        item.StatusTime = (new Date(item.StatusTime)).getTime();
-
-        // DynamoDB expects a JSON map with PutRequest
-        //  - Perform a PutItem operation on the specified item.
-        //   The item to be put is identified by an Item subelement:
-        //   - Item - A map of attributes and their values.
-        return {
-            PutRequest: {
-                Item: item,
-            },
-        };
-    });
-    // returns a Promise object that is resolved with the given value (requestItems)
-    return Promise.resolve(requestItems);
-}
-
-// Take the correctly formatted items in the request,
-// process them in batches of 25 at the time
-function processRequests(requestItems) {
-    const batches = [];
-
-    while (requestItems.length > 0) {
-        // process 25 records at the time
-        console.log('remaining records to be processed: ' + requestItems.length );
-        batches.push(writeRecords(requestItems.splice(0, 25)));
-    }
-    // makes sure all are processed
-    return Promise.all(batches);
-}
-
-// Write to DynamoDb the requestItems (in JSON)
-// handle Throttling erros
-function writeRecords(requestItems) {
-    const params = {
-        RequestItems: {
-            [TABLE_NAME]: requestItems,
-        },
+    const dynamoItem = {
+        Username: event.userId,
+        s3key: event.s3Key,
+        s3bucket: event.s3Bucket
     };
 
-    return new Promise((resolve, reject) => {
+    var indexDetails = event['parallelResult'][0];
+    var thumbnailDetails = event['parallelResult'][1];
+    dynamoItem['faceId'] = indexDetails['FaceId'];
+    dynamoItem['thumbnail'] = thumbnailDetails['thumbnail'];
+    docClient.put({
+        TableName: tableName,
+        Item: dynamoItem
+        // uncomment below if you want to disallow overwriting if the user is already in the table
+        //   ,ConditionExpression: 'attribute_not_exists (Username)'
+    }).promise().then(data => {
+        callback(null, data);
+    }).catch(err => {
+        callback(err);
+    })
 
-        // DynamoDB is busy so we need to slow down and retry
-        function retry(retryRequestItems) {
-            const delay = ((Math.random() * (3 - 1)) + 1) * 1000;
-
-            console.log(`Retrying ${retryRequestItems.length} reqs in ${Math.round(delay)}ms`);
-            setTimeout(() => {
-                writeRecords(retryRequestItems).then(resolve).catch(reject);
-            }, delay);
-        }
-
-        // The logic for DynamoDB.batchWrite() is funky, make sure
-        // you read the documentation:
-        // http://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_BatchWriteItem.html
-        // because you may get errors for unprocessed data and for throttling
-        DynamoDB.batchWrite(params, (err, data) => {
-            if (data && data.UnprocessedItems[TABLE_NAME]) {
-                retry(data.UnprocessedItems[TABLE_NAME]);
-            } else if (err && THROTTLING_ERRORS.includes(err.name)) {
-                retry(requestItems);
-            } else if (err) {
-                reject(err);
-            } else {
-                // all went well
-                resolve();
-            }
-        });
-    });
 }
--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 Collection of code samples to do a bunch of stuff with lambda and other AWS services
 
-- [S3toDDBwithLambda.js](/DataProcessing/S3toDDBwithLambda.js): Takes a JSON file in an S3 bucket and performs `batchWrite()` to a table in DynamoDB.  The logic for DynamoDB.batchWrite() is funky, make sure you read the [documentation](http://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_BatchWriteItem.html) because you may get errors for unprocessed data and for throttling. Also remember to setup the correct trigger in lambda for S3 and use the right IAM roles. More details [here](https://github.com/awslabs/aws-serverless-workshops/tree/master/DataProcessing/1_FileProcessing)
+- [BatchWriteToDDBwithLambda.js](/DataProcessing/BatchWriteToDDBwithLambda.js): Takes a file with lots of JSON elements in an S3 bucket and performs `batchWrite()` to a table in DynamoDB.  The logic for DynamoDB.batchWrite() is funky, make sure you read the [documentation](http://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_BatchWriteItem.html) because you may get errors for unprocessed data and for throttling. Also remember to setup the correct trigger in lambda for S3 and use the right IAM roles. More details [here](https://github.com/awslabs/aws-serverless-workshops/tree/master/DataProcessing/1_FileProcessing)
+
+- [WriteToDDBwithLambda.js](/DataProcessing/WriteToDDBwithLambda.js): // simple and straightforward function to write to DynamoDB a single  object `dynamoItem` using `AWS.DynamoDB.DocumentClient()`. DocumentClient  annotates native JavaScript types supplied as input parameters, as well as converts annotated response data to native JavaScript types.
+
